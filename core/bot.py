7c0a754d26e0620f6daf1eb98814d54b85e5fd34
Sokhibjon Orzikulov <genemators@gmail.com>
2020-05-25 21:54:54 +0500

    Update bot.py

--- core/bot.py
+++ core/bot.py
@@ -34,18 +34,18 @@ class WebhookServer(object):
 
 
 def launch():
-    # bot.remove_webhook()
-    # bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
-    #                 certificate=open(WEBHOOK_SSL_CERT, 'r'))
-    #
-    # cherrypy.config.update({
-    #     'server.socket_host': WEBHOOK_LISTEN,
-    #     'server.socket_port': WEBHOOK_PORT,
-    #     'server.ssl_module': 'builtin',
-    #     'server.ssl_certificate': WEBHOOK_SSL_CERT,
-    #     'server.ssl_private_key': WEBHOOK_SSL_PRIV
-    # })
-    # cherrypy.quickstart(WebhookServer(), WEBHOOK_URL_PATH, {'/': {}})
+    bot.remove_webhook()
+    bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
+                    certificate=open(WEBHOOK_SSL_CERT, 'r'))
 
-    bot.polling(True)
+    cherrypy.config.update({
+        'server.socket_host': WEBHOOK_LISTEN,
+        'server.socket_port': WEBHOOK_PORT,
+        'server.ssl_module': 'builtin',
+        'server.ssl_certificate': WEBHOOK_SSL_CERT,
+        'server.ssl_private_key': WEBHOOK_SSL_PRIV
+    })
+    cherrypy.quickstart(WebhookServer(), WEBHOOK_URL_PATH, {'/': {}})
+
+    # bot.polling(True)
 

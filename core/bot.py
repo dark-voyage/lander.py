b7b3ed95dc6e841c9a63122579424293672cace2
Sokhibjon Orzikulov <genemators@gmail.com>
2020-05-25 20:33:40 +0500

    Update bot.py

--- core/bot.py
+++ core/bot.py
@@ -7,7 +7,7 @@ from config import token
 bot = telebot.AsyncTeleBot(token)
 logger = telebot.logger
 
-WEBHOOK_HOST = 'IP-адрес сервера, на котором запущен бот'
+WEBHOOK_HOST = 'https://denolander.herokuapp.com/'
 WEBHOOK_PORT = 443  # 443, 80, 88 или 8443 (порт должен быть открыт!)
 WEBHOOK_LISTEN = '0.0.0.0'  # На некоторых серверах придется указывать такой же IP, что и выше
 
@@ -15,7 +15,7 @@ WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Путь к сертификату
 WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Путь к приватному ключу
 
 WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
-WEBHOOK_URL_PATH = "/%s/" % (config.token)
+WEBHOOK_URL_PATH = "/%s/" % token
 
 
 class WebhookServer(object):

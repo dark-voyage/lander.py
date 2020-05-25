66d8c0135608d5849789fbac2694a3f3eca0930a
Sokhibjon Orzikulov <genemators@gmail.com>
2020-05-25 20:11:44 +0500

    Start

--- action/help.py
+++ action/help.py
@@ -1,3 +1,4 @@
+from telebot import types
 from core import bot
 
 
@@ -5,5 +6,27 @@ def _help():
     @bot.message_handler(commands=['help'])
     def __help(message):
         _from = message.from_user.id
-        bot.send_message(_from, "Help message activated")
-        bot.register_next_step_handler()
+        buttons = [
+            types.InlineKeyboardButton(
+                text="Website",
+                url="https://deno.land/"
+            ),
+            types.InlineKeyboardButton(
+                text="Documentation",
+                url="https://doc.deno.land/"
+            ),
+            types.InlineKeyboardButton(
+                text="Start searching packages",
+                switch_inline_query_current_chat=""
+            )
+        ]
+        keyboard = types.InlineKeyboardMarkup(row_width=len(buttons))
+        for button in buttons:
+            keyboard.add(button)
+        helper = \
+            f"<b>This is the available command list:</b> \n" \
+            f"/help - to show this menu \n" \
+            f"\n" \
+            f"<i>In order to use package search engine, press 'start searching packages' button below...</i>"
+
+        bot.send_message(_from, helper, reply_markup=keyboard, parse_mode="HTML")
--- config.py
+++ config.py
@@ -6,9 +6,9 @@ import os
 # This is the token for Telegram Bot API
 # Please be carefull while publishing your
 # own Telegram Bot API. Others might use it
-token = os.environ.get("BOT_TOKEN") or "1281040995:AAHxeRiZsH2TQ3VLo0ZyZSC1sBS3RIwWYGs"
+token = os.environ.get("BOT_TOKEN")
 
 # This is the link of live database json file
 # bot will download the file and proceed with
 # operations given on bot's program.
-url = os.environ.get("URL") or "https://raw.githubusercontent.com/denoland/deno_website2/master/database.json"
+url = os.environ.get("URL")

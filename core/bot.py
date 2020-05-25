64bbd0a58829061734dde60040a39fe227be996a
Sokhibjon Orzikulov <genemators@gmail.com>
2020-05-25 20:18:00 +0500

    Fixed Webhook

--- core/bot.py
+++ core/bot.py
@@ -1,6 +1,5 @@
 import telebot
 import logging
-import cherrypy
 
 from config import token
 
@@ -8,40 +7,10 @@ bot = telebot.AsyncTeleBot(token)
 logger = telebot.logger
 
 
-WEBHOOK_HOST = 'IP-адрес сервера, на котором запущен бот'
-WEBHOOK_PORT = 443
-WEBHOOK_LISTEN = '0.0.0.0'
-WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
-WEBHOOK_URL_PATH = "/%s/" % token
-
-
-class WebhookServer(object):
-    @cherrypy.expose
-    def index(self):
-        if 'content-length' in cherrypy.request.headers and \
-                        'content-type' in cherrypy.request.headers and \
-                        cherrypy.request.headers['content-type'] == 'application/json':
-            length = int(cherrypy.request.headers['content-length'])
-            json_string = cherrypy.request.body.read(length).decode("utf-8")
-            update = telebot.types.Update.de_json(json_string)
-            # Эта функция обеспечивает проверку входящего сообщения
-            bot.process_new_updates([update])
-            return ''
-        else:
-            raise cherrypy.HTTPError(403)
-
-
 def launch():
     try:
-        bot.remove_webhook()
-        bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH)
-
-        cherrypy.config.update({
-            'server.socket_host': WEBHOOK_LISTEN,
-            'server.socket_port': WEBHOOK_PORT
-        })
-
-        cherrypy.quickstart(WebhookServer(), WEBHOOK_URL_PATH, {'/': {}})
+        bot.polling(True)
+        logger(logging.DEBUG)
     except ConnectionRefusedError:
         print("Error occurred while starting app...")
     pass
